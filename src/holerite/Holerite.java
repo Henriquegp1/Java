bbe950c9611680c0171a3342c32a74975468c044
Henrique <henriquegp17@gmail.com>
2025-06-03 22:31:59 -0300

    Primeira versão do holerite

---
+++ src/holerite/Holerite.java
@@ -0,0 +1,18 @@
+
+package holerite;
+
+
+public class Holerite {
+
+    
+    public static void main(String[] args) {
+        
+        calCulos bolo = new calCulos();
+        
+        bolo.entrarNome();
+        bolo.entrarSalarioAtual();
+        bolo.entrarTx_inss();
+        bolo.exibirHolerite();
+    }
+    
+}
